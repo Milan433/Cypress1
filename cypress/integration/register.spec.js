01db231f5eea877152b4e33f6f19130f85e2d931
Milan433 <liman1234ns@hotmail.rs>
2022-03-01 12:23:22 +0100

    format code

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -3,18 +3,19 @@
 describe('login test', () => {
     it('visit gallery page', () => {
         cy.visit('/');
-    },);});
+    });
+});
 
 
-    it('click on the register button' , () => {
-        cy.visit("/");
-        cy.get('.nav-link').eq(2).click();
-        cy.get('#first-name').type('nikola');
-        cy.get('#last-name').type('nikolic');
-        cy.get('#email').type('nikola@nikolic.com');
-        cy.get('#password').type('nikola1234');
-        cy.get('#password-confirmation').type('nikola1234');
-        cy.get('.form-check-input').click();
-        cy.get('button').click();
+it('click on the register button', () => {
+    cy.visit("/");
+    cy.get('.nav-link').eq(2).click();
+    cy.get('#first-name').type('nikola');
+    cy.get('#last-name').type('nikolic');
+    cy.get('#email').type('nikola@nikolic.com');
+    cy.get('#password').type('nikola1234');
+    cy.get('#password-confirmation').type('nikola1234');
+    cy.get('.form-check-input').click();
+    cy.get('button').click();
 
-    })
+});
